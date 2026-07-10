54fd041c4ca9cd9c5e80bbaba11ed77ccc0315b9
CursedCodMan <104089072+RngValue@users.noreply.github.com>
2026-07-10 18:13:55 +0200

    notes :3

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Note from "@/components/note.component";
+
 export default function Home() {
     return (
         <>
             <h1 className="text-center pt-6 pb-6">WOW</h1>
+            <Note note={1} transpose={0}></Note>
         </>
     )
 }
---
+++ components/note.component.tsx
@@ -0,0 +1,31 @@
+import { Button } from "./ui/button";
+import { Card, CardAction, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+
+type whatIExpect = {
+    note: number,
+    transpose: number
+}
+
+export default function Note(props: whatIExpect) {
+    const TOTAL_NOTES = 12
+    const notesArray = ['C', 'C♯ D♭', 'D', 'D♯ E♭', 'E', 'F', 'F♯ G♭', 'G', 'G♯ A♭', 'A', 'A♯ B♭/H♭', 'B/H']
+
+    let realNote = (props.note + props.transpose) % TOTAL_NOTES - 1
+
+    return <>
+        <Card className="relative mx-auto w-full max-w-sm pt-0">
+            <CardHeader>
+                <CardTitle className="mt-4">{ notesArray[realNote] }</CardTitle>
+                <CardDescription>
+                    A practical talk on component APIs, accessibility, and shipping
+                    faster.
+                </CardDescription>
+            </CardHeader>
+            <img
+                src="https://avatar.vercel.sh/shadcn1"
+                alt="Event cover"
+                className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+            />
+        </Card>
+    </>
+}
